719755a9274bd17ac6b060a2da2e083506d4b5ab
Shreyas <skraman1999@gmail.com>
2014-07-21 21:48:05 -0700

    Better printing. XBee's and PID coming up.

--- src/quadcopter.c
+++ src/quadcopter.c
@@ -7,13 +7,13 @@
 int main()
 {
   waitcnt(CNT + CLKFREQ); // Wait one second.
-  
+
   imuInit();
   while(1)
   {
     waitcnt(CNT + CLKFREQ/10);
     imuUpdate();
-    printf("G: %6d %6d %6d\t", gx, gy, gz);
+    printf("G: %6d %6d %6d    ", gx, gy, gz);
     printf("A: %6d %6d %6d\n", ax, ay, az);
   }
 }
