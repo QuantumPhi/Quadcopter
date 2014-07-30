a2d7f73c93026282fe5799f59336a335dfe16e41
Shreyas <skraman1999@gmail.com>
2014-07-30 13:24:29 -0700

    Just does IMU stuff.

--- src/quadcopter.c
+++ src/quadcopter.c
@@ -6,20 +6,15 @@
 
 int main()
 {
-  xbeeInit();
-
-  cog_run(&motorRun, 0);
-
-  //imuInit();
-  //cog_run(&imuRun, 1);
+  imuInit();
+  cog_run(&imuRun, 1);
   signed short gx,gy,gz,ax,ay,az;
   
-  //while(1)
-  //{
-  //  waitcnt(CNT + CLKFREQ/10);
-  //  getImuData(&gx, &gy, &gz, &ax, &ay, &az);
-  //  //gx = computePID(gx, 0);
-  //  printf("G: %5d %5d %5d\t", gx, gy, gz);
-  //  printf("A: %5d %5d %5d\n", ax, ay, az);
-  //}
+  while(1)
+  {
+    waitcnt(CNT + CLKFREQ/10);
+    getImuData(&gx, &gy, &gz, &ax, &ay, &az);
+    printf("G: %5d %5d %5d\t", gx, gy, gz);
+    printf("A: %5d %5d %5d\n", ax, ay, az);
+  }
 }
